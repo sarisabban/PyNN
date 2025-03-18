ce16708f5bb992fa729d886156a7ad73c4db3d9a
Sari Sabban <sari.sabban@gmail.com>
2025-03-18 09:20:30 +0300

    Start building classes PyNN.py

--- PyNN.py
+++ PyNN.py
@@ -2,26 +2,146 @@ import math
 import numpy as np
 np.random.seed(42)
 
-#----- Import Data -----#
-def spiral_data(samples, classes):
-    X = np.zeros((samples*classes, 2))
-    Y = np.zeros(samples*classes, dtype='uint8')
-    for class_n in range(classes):
-        ix = range(samples*class_n, samples*(class_n+1))
-        r=np.linspace(0.0, 1, samples)
-        t=np.linspace(class_n*4,(class_n+1)*4,samples)+np.random.randn(samples)*0.2
-        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
-        Y[ix] = class_n
-    return(X, Y)
 
-def sine_data(samples=1000):
-    X = np.arange(samples).reshape(-1, 1) / samples
-    Y = np.sin(2 * np.pi * X).reshape(-1, 1)
-    return(X, Y)
 
-def Dense(n_inputs, n_outputs, x, w, b):# A single Dense layer
-	z = np.dot(x, w) + b
-	return(z, w, b)
+
+def Parameters(inputs=1, outputs=1, alg='he uniform', sd=0.1, a=-0.5, b=0.5):
+	''' Parameter Initialisation '''
+	if alg == 'zeros':
+		w = np.zeros((inputs, outputs))
+	elif alg == 'ones':
+		w = np.ones((inputs, outputs))
+	elif alg == 'random normal':
+		w = np.random.normal(loc=0.0, scale=sd, size=(inputs, outputs))
+	elif alg == 'random uniform':
+		w = np.random.uniform(low=a, high=b, size=(inputs, outputs))
+	elif alg == 'glorot normal':
+		sd = 1 / (math.sqrt(inputs + outputs))
+		w =  np.random.normal(loc=0.0, scale=sd, size=(inputs, outputs))
+	elif alg == 'glorot uniform':
+		a = - math.sqrt(6) / (math.sqrt(inputs + outputs))
+		b = math.sqrt(6) / (math.sqrt(inputs + outputs))
+		w = np.random.uniform(low=a, high=b, size=(inputs, outputs))
+	elif alg == 'he normal':
+		sd = 2 / (math.sqrt(inputs))
+		w = np.random.normal(loc=0.0, scale=sd, size=(inputs, outputs))
+	elif alg == 'he uniform':
+		a = - math.sqrt(6) / (math.sqrt(inputs))
+		b = math.sqrt(6) / (math.sqrt(inputs))
+		w = np.random.uniform(low=a, high=b, size=(inputs, outputs))
+	b = np.zeros((1, outputs))
+	gamma = np.ones((1, outputs))
+	beta = np.zeros((1, outputs))
+	return(w, b, gamma, beta)
+
+
+
+
+class PyNN():
+	''' Lightweight NumPy-based neural network library '''
+	def __init__(self):
+		''' Initialise the class with the following '''
+		self.layers = []
+	def add(self, layer):
+		''' Add an object to the network '''
+		self.layers.append(layer)
+#	def set(self, *, loss, optimiser):
+#		''' Set the loss and the optimiser '''
+#		self.loss = loss
+#		self.optimiser = optimiser
+#	def train(self, X, Y, epochs=1, verbose=0):
+#		''' Train the network, forward pass followed by backward pass '''
+#	def show(self):
+#		''' Print out the structure of the network '''
+#	def verbosity(self):
+#		''' Control level of information printout during training '''
+#	def load(self, path):
+#		''' Load model '''
+#	def save(self, path):
+#		''' Save model '''
+#		with open(path, 'wb') as f:
+#		pickle.dump(self.w, f)
+#	def batch(self):
+#		''' Setup mini-batches '''
+#		steps = X.shape[0] // batch_size
+#	def evaluate(self):
+#	''' Train on training set, then validate on valid, after training test of test set '''
+#	def checkpoint(self):
+#	''' Add a training checkpoint '''
+#	def predict(self):
+#	''' Perform a prediction '''
+#	def GPU(self):
+#	''' Use GPU instead of CPU '''
+
+# test in MNIST and fasion-MNIST
+
+
+
+
+
+
+
+
+class Dense():
+	def __init__(self, inputs=1, outputs=1):
+		''' Initialise parameters '''
+		self.w, self.b, self.gamma, self.beta = Parameters(inputs, outputs)
+	def forward(self, x):
+		''' A dense layer '''
+		self.z = np.dot(x, self.w) + self.b
+#	def backward(self):
+
+class ReLU():
+	''' The ReLU activation function '''
+	def forward(self, z):
+		self.y = np.maximum(0, z)
+#	def backward(self):
+
+class Linear():
+	''' The Linear activation function '''
+	def forward(self, z):
+		self.y = z
+#	def backward(self):
+
+class Softmax():
+	''' The Softmax activation function '''
+	def forward(self, z):
+		exp_values = np.exp(z - np.max(z, axis=1, keepdims=True))
+		self.y = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+#	def backward(self):
+
+class Sigmoid():
+	''' The Sigmoid activation function '''
+	def forward(self, z):
+		self.y = 1 / (1 + np.exp(-z))
+#	def backward(self):
+
+#class TanH():
+#	''' The TanH activation function '''
+#	def forward(self, z):
+#	def backward(self):
+#class Step():
+#	''' The Step activation function '''
+#	def forward(self, z):
+#	def backward(self):
+
+
+
+
+model = PyNN()
+model.add(Dense(1, 64))
+model.add(ReLU())
+model.add(Dense(64, 64))
+model.add(ReLU())
+model.add(Dense(64, 1))
+model.add(Linear())
+print(model.layers[0].w.shape)
+
+
+
+
+
+
 
 def d_Dense(y, DA_Dz, w):# The derivative of a single Dense layer
 	dL_dw = np.dot(y.T, DA_Dz)
@@ -29,19 +149,12 @@ def d_Dense(y, DA_Dz, w):# The derivative of a single Dense layer
 	dL_dx = np.dot(DA_Dz, w.T)
 	return(dL_dw, dL_db, dL_dx)
 
-def ReLU(z):# The ReLU activation function
-	y = np.maximum(0, z)
-	return(y)
 
 def d_ReLU(DL_DY, z):# The derivative of the ReLU activation function
 	DR_Dz = DL_DY.copy()
 	DR_Dz[z <= 0] = 0
 	return(DR_Dz)
 
-def Softmax(x):# The Softmax activation function
-	exp_values = np.exp(x - np.max(x, axis=1, keepdims=True))
-	y = exp_values / np.sum(exp_values, axis=1, keepdims=True)
-	return(y)
 
 def d_Softmax(DL_DY, y_pred):# Derivative of the Softmax activation function
 	DS_Dz = np.empty_like(DL_DY)
@@ -63,9 +176,6 @@ def d_CCE_Loss(y_true, y_pred):# Derivative of Categorical Cross-Entropy loss
 	DL_DY = (-y_true / y_pred) / len(y_pred)
 	return(DL_DY)
 
-def Sigmoid(z):# The Sigmoid activation function
-	y = 1 / (1 + np.exp(-z))
-	return(y)
 
 def d_Sigmoid(DA_Dz, y_pred):# The derivative of the Sigmoid activation function
 	DS_Dz = DA_Dz * (1 - y_pred) * y_pred
@@ -145,10 +255,6 @@ def d_Dropout(p, DA_Dz):# Dropout layer - derivative
 	DA_Dz *= np.random.binomial(1, 1-p, DA_Dz.shape) / (1-p)
 	return(DA_Dz)
 
-def Linear(z):# The Linear activation function
-	y = z
-	return(y)
-
 def d_Linear(DL_DY):# The derivative of the Linear activation function
 	DA_Dz = DL_DY.copy()
 	return(DA_Dz)
@@ -175,34 +281,6 @@ def Regression_Accuracy(y_true, y_pred):# Measure the accuracy
 	accuracy = np.mean(np.absolute(predictions - y_true) < accuracy_precision)
 	return(accuracy)
 
-def Parameters(inputs=3, outputs=3, alg='he uniform', sd=0.1, a=-0.5, b=0.5):
-	if alg == 'zeros':
-		w = np.zeros((inputs, outputs))
-	elif alg == 'ones':
-		w = np.ones((inputs, outputs))
-	elif alg == 'random normal':
-		w = np.random.normal(loc=0.0, scale=sd, size=(inputs, outputs))
-	elif alg == 'random uniform':
-		w = np.random.uniform(low=a, high=b, size=(inputs, outputs)) 
-	elif alg == 'glorot normal':
-		sd = 1 / (math.sqrt(inputs + outputs))
-		w =  np.random.normal(loc=0.0, scale=sd, size=(inputs, outputs))
-	elif alg == 'glorot uniform':
-		a = - math.sqrt(6) / (math.sqrt(inputs + outputs))
-		b = math.sqrt(6) / (math.sqrt(inputs + outputs))
-		w = np.random.uniform(low=a, high=b, size=(inputs, outputs))
-	elif alg == 'he normal':
-		sd = 2 / (math.sqrt(inputs))
-		w = np.random.normal(loc=0.0, scale=sd, size=(inputs, outputs))
-	elif alg == 'he uniform':
-		a = - math.sqrt(6) / (math.sqrt(inputs))
-		b = math.sqrt(6) / (math.sqrt(inputs))
-		w = np.random.uniform(low=a, high=b, size=(inputs, outputs))
-	b = np.zeros((1, outputs))
-    gamma = np.ones((1, outputs))
-    beta = np.zeros((1, outputs))
-	return(w, b, gamma, beta)
-
 def BatchNorm(z, g, b, e=1e-7):
 	mean = np.mean(z, axis=0, keepdims=True)
 	var = np.var(z, axis=0, keepdims=True)
@@ -221,139 +299,14 @@ def d_BatchNorm(DA_Dz, z_norm, cache):
 
 
 
-# Multi class
-#X, Y = spiral_data(samples=100, classes=3) # X=(300, 2)   Y=(300,)   3 Classes
-# Binary class
-#X, Y = spiral_data(samples=100, classes=2) # X = (200, 2)   y = (200,)
-# Regression
-#X, Y = sine_data() # X = (1000, 1)   Y = (1000, 1)
 
 
 
-#----- Training -----#
-"""
-# Multi-Classification
-w1 = 0.01 * np.random.randn(2, 64)
-b1 = np.zeros((1, 64))
-w2 = 0.01 * np.random.randn(64, 3)
-b2 = np.zeros((1, 3))
 
-w1_m = np.zeros_like(w1)
-w1_c = np.zeros_like(w1)
-b1_m = np.zeros_like(b1)
-b1_c = np.zeros_like(b1)
-w2_m = np.zeros_like(w2)
-w2_c = np.zeros_like(w2)
-b2_m = np.zeros_like(b2)
-b2_c = np.zeros_like(b2)
+def sine_data(samples=1000):
+	X = np.arange(samples).reshape(-1, 1) / samples
+	Y = np.sin(2 * np.pi * X).reshape(-1, 1)
+	return(X, Y)
 
-for epoch in range(1000):
-	y_true = Y
-	z1, w1, b1 = Dense(2, 64, X, w1, b1)
-	y1 = ReLU(z1)
-	y1 = Dropout(0.1, y1) # dropout
-	z2, w2, b2 = Dense(64, 3, y1, w2, b2)
-	y_pred = Softmax(z2)
-	reg1 = L1L2(w1, b1, 0, 0, 5e-4, 5e-4) # L1 + L2
-	reg2 = L1L2(w2, b2, 0, 0, 0, 0) # L1 + L2
-	loss = CCE_Loss(y_true, y_pred)
-	cost, accuracy = Cost(loss) + reg1 + reg2, Accuracy(y_true, y_pred)
-	cost, accuracy = Cost(loss), Categorical_Accuracy(y_true, y_pred)
-	DL_DY = d_CCE_Loss(y_true, y_pred)
-	DS_Dz2 = d_Softmax(DL_DY, y_pred)
-	dL_dwL2, dL_dbL2, DS_Dy1 = d_Dense(y1, DS_Dz2, w2)
-	DS_Dy1 = d_Dropout(0.1, DS_Dy1)
-	dL_dwL2, dL_dbL2 = d_L1L2(w2, b2, dL_dwL2, dL_dbL2, 0, 0, 0, 0) # L1 + L2
-	DR_Dz1 = d_ReLU(DS_Dy1, z1)
-	dL_dwL1, dL_dbL1, dL_dx = d_Dense(X, DR_Dz1, w1)
-	dL_dwL1, dL_dbL1 = d_L1L2(w1, b1, dL_dwL1, dL_dbL1, 0, 0, 5e-4, 5e-4) # L1 + L2
-	w1, b1, w1_m, w1_c, b1_m, b1_c = Adam(0.05, 5e-7, 0.9, 0.999, 1e-7, w1, b1, dL_dwL1, dL_dbL1, epoch+1, w1_m, w1_c, b1_m, b1_c)
-	w2, b2, w2_m, w2_c, b2_m, b2_c = Adam(0.05, 5e-7, 0.9, 0.999, 1e-7, w2, b2, dL_dwL2, dL_dbL2, epoch+1, w2_m, w2_c, b2_m, b2_c)
-	print(f'epoch: {epoch+1} --- cost: {cost:.7f} --- accuracy: {accuracy:.3f}')
-"""
-
-
-"""
-# Binary-Classification
-w1 = 0.01 * np.random.randn(2, 64)
-b1 = np.zeros((1, 64))
-w2 = 0.01 * np.random.randn(64, 1)
-b2 = np.zeros((1, 1))
-
-w1_m = np.zeros_like(w1)
-w1_c = np.zeros_like(w1)
-b1_m = np.zeros_like(b1)
-b1_c = np.zeros_like(b1)
-w2_m = np.zeros_like(w2)
-w2_c = np.zeros_like(w2)
-b2_m = np.zeros_like(b2)
-b2_c = np.zeros_like(b2)
-
-Y = Y.reshape(-1, 1) # y = (200, 1)
-for epoch in range(1):
-	y_true = Y
-	z1, w1, b1 = Dense(2, 64, X, w1, b1)
-	y1 = ReLU(z1)
-	z2, w2, b2 = Dense(64, 1, y1, w2, b2)
-	y_pred = Sigmoid(z2)
-	reg1 = L1L2(w1, b1, 0, 0, 5e-4, 5e-4) # L1 + L2
-	reg2 = L1L2(w2, b2, 0, 0, 0, 0) # L1 + L2
-	loss = BCE_Loss(y_true, y_pred)
-	cost, accuracy = Cost(loss) + reg1 + reg2, Binary_Accuracy(y_true, y_pred)
-	DL_DY = d_BCE_Loss(y_true, y_pred)
-	DS_Dz2 = d_Sigmoid(DL_DY, y_pred)
-	dL_dwL2, dL_dbL2, DS_Dy1 = d_Dense(y1, DS_Dz2, w2)
-	dL_dwL2, dL_dbL2 = d_L1L2(w2, b2, dL_dwL2, dL_dbL2, 0, 0, 0, 0) # L1 + L2
-	DR_Dz1 = d_ReLU(DS_Dy1, z1)
-	dL_dwL1, dL_dbL1, dL_dx = d_Dense(X, DR_Dz1, w1)
-	dL_dwL1, dL_dbL1 = d_L1L2(w1, b1, dL_dwL1, dL_dbL1, 0, 0, 5e-4, 5e-4) # L1 + L2
-	w1, b1, w1_m, w1_c, b1_m, b1_c = Adam(0.05, 5e-7, 0.9, 0.999, 1e-7, w1, b1, dL_dwL1, dL_dbL1, epoch+1, w1_m, w1_c, b1_m, b1_c)
-	w2, b2, w2_m, w2_c, b2_m, b2_c = Adam(0.05, 5e-7, 0.9, 0.999, 1e-7, w2, b2, dL_dwL2, dL_dbL2, epoch+1, w2_m, w2_c, b2_m, b2_c)
-	print(f'epoch: {epoch+1} --- cost: {cost:.3f} --- accuracy: {accuracy:.3f}')
-"""
-
-
-"""
-# Regression
-w1 = 0.01 * np.random.randn(1, 64)
-b1 = np.zeros((1, 64))
-w2 = 0.01 * np.random.randn(64, 64)
-b2 = np.zeros((1, 64))
-w3 = 0.01 * np.random.randn(64, 1)
-b3 = np.zeros((1, 1))
-
-w1_m = np.zeros_like(w1)
-w1_c = np.zeros_like(w1)
-b1_m = np.zeros_like(b1)
-b1_c = np.zeros_like(b1)
-w2_m = np.zeros_like(w2)
-w2_c = np.zeros_like(w2)
-b2_m = np.zeros_like(b2)
-b2_c = np.zeros_like(b2)
-w3_m = np.zeros_like(w3)
-w3_c = np.zeros_like(w3)
-b3_m = np.zeros_like(b3)
-b3_c = np.zeros_like(b3)
-
-for epoch in range(5000):
-	y_true = Y
-	z1, w1, b1 = Dense(2, 64, X, w1, b1)
-	y1 = ReLU(z1)
-	z2, w2, b2 = Dense(64, 64, y1, w2, b2)
-	y2 = ReLU(z2)
-	z3, w3, b3 = Dense(64, 1, y2, w3, b3)
-	y_pred = Linear(z3)
-	loss = MSE_Loss(y_true, y_pred)
-	cost, accuracy = Cost(loss), Regression_Accuracy(y_true, y_pred)
-	DL_DY = d_MSE_Loss(y_true, y_pred)
-	DS_Dz3 = d_Linear(DL_DY)
-	dL_dwL3, dL_dbL3, DS_Dy2 = d_Dense(y2, DS_Dz3, w3)
-	DR_Dz2 = d_ReLU(DS_Dy2, z2)
-	dL_dwL2, dL_dbL2, DS_Dy1 = d_Dense(y1, DR_Dz2, w2)
-	DR_Dz1 = d_ReLU(DS_Dy1, z1)
-	dL_dwL1, dL_dbL1, dL_dx = d_Dense(X, DR_Dz1, w1)
-	w1, b1, w1_m, w1_c, b1_m, b1_c = Adam(0.005, 1e-3, 0.9, 0.999, 1e-7, w1, b1, dL_dwL1, dL_dbL1, epoch+1, w1_m, w1_c, b1_m, b1_c)
-	w2, b2, w2_m, w2_c, b2_m, b2_c = Adam(0.005, 1e-3, 0.9, 0.999, 1e-7, w2, b2, dL_dwL2, dL_dbL2, epoch+1, w2_m, w2_c, b2_m, b2_c)
-	w3, b3, w3_m, w3_c, b3_m, b3_c = Adam(0.005, 1e-3, 0.9, 0.999, 1e-7, w3, b3, dL_dwL3, dL_dbL3, epoch+1, w3_m, w3_c, b3_m, b3_c)
-	print(f'epoch: {epoch+1} --- cost: {cost:.3f} --- accuracy: {accuracy:.3f}')
-"""
+X, Y = sine_data()
+print(X.shape, Y.shape)
