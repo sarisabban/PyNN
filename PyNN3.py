3eee9b1511ddf65da540a9694a547d3f5e23956e
slurm <sari.sabban@gmail.com>
2025-03-23 20:45:35 +0300

    Added more functions

--- PyNN3.py
+++ PyNN3.py
@@ -6,80 +6,189 @@ class PyNN():
 	''' Lightweight NumPy-based neural network library '''
 	#---------- Utilities ---------- #
 	def __init__(self):
-		''' Initialise the class with the following '''
+		''' Initialise the class with the following objects '''
 		self.layers = []
 	def add(self, layer):
-		''' Add an object to the network '''
+		''' Add a layer to the network '''
 		self.layers.append(layer)
 	def cost(self, loss):
 		''' The cost function '''
 		return(np.mean(loss))
 	def verbosity(self, epoch, cost, accuracy, *, verbose=1):
 		''' Control level of information printout during training '''
-		E = epoch
-		C = cost
-		A = accuracy
+		E, C, A = epoch, cost, accuracy
 		string = f'Epoch: {E:,} | Cost: {C:.5f} | Accuracy: {A:.5f}'
 		if verbose == 0:
 			pass
 		elif verbose == 1:
-			if epoch % 100 == 0:
-				print(string)
+			if epoch % 100 == 0: print(string)
 		elif verbose == 2:
 			print(string)
-	#---------- Accuracy Functions ---------- #
-	def Binary_Accuracy(self, y_true, y_pred):
-		predictions = (y_pred > 0.5) * 1
-		accuracy = np.mean(predictions==y_true)
-		return(accuracy)
-	#---------- Optimisers ---------- #
-	def SGD(self, lr, layer):
-		layer.w -= lr * layer.dL_dw
-		layer.b -= lr * layer.dL_db
-	#---------- Loss Functions ---------- #
-	class BCE_Loss():
-		''' The Binary Cross-Entropy loss function '''
-		def forward(self, y_true, y_pred):
-			''' The forward pass '''
-			y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
-			loss = -(y_true*np.log(y_pred) + (1-y_true) * np.log(1-y_pred))
-			return(loss)
-		def backward(self, y_true,  y_pred):
-			''' The backward pass '''
-			y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
-			DL_DY = (y_pred-y_true) / (y_pred * (1-y_pred)) / len(y_pred)
-			return(DL_DY)
+
+
+#	def show(self):
+#		''' Print out the structure of the network '''
+#	def load(self, path):
+#		''' Load model '''
+#	def save(self, path):
+#		''' Save model '''
+#		with open(path, 'wb') as f:
+#		pickle.dump(self.w, f)
+#	def predict(self):
+#	''' Perform a prediction '''
+#	def GPU(self):
+#	''' Use GPU instead of CPU '''
+
+
+
+
+
+
 	#---------- Activation Functions ----------#
-	class ReLU():
-		''' The ReLU activation function '''
+	class Step():
+		''' The Step activation function (for binary classification) '''
 		def forward(self, z):
-			''' The forward pass '''
-			self.z = z
-			y = np.maximum(0, self.z)
-			return(y)
+			self.y = np.where(z >= 0, 1, 0)
+			return self.y
 		def backward(self, DL_DY):
-			''' The backward pass '''
+			return(np.zeros_like(DL_DY))
+	class Linear():
+		''' The Linear activation function '''
+		def forward(self, z):
+			self.y = z
+			return(self.y)
+		def backward(self, DL_DY):
 			DA_Dz = DL_DY.copy()
-			DA_Dz[self.z <= 0] = 0
 			return(DA_Dz)
 	class Sigmoid():
 		''' The Sigmoid activation function '''
 		def forward(self, z):
-			''' The forward pass '''
 			self.y = 1 / (1 + np.exp(-z))
 			return(self.y)
 		def backward(self, DA_Dz):
-			''' The backward pass '''
 			DS_Dz = DA_Dz * (1 - self.y) * self.y
 			return(DS_Dz)
+	class ReLU():
+		''' The ReLU activation function '''
+		def forward(self, z):
+			self.z = z
+			y = np.maximum(0, self.z)
+			return(y)
+		def backward(self, DL_DY):
+			DA_Dz = DL_DY.copy()
+			DA_Dz[self.z <= 0] = 0
+			return(DA_Dz)
+	class LeakyReLU():
+		''' The LeakyReLU activation function '''
+		def __init__(self, alpha=0.01):
+			self.alpha = alpha
+		def forward(self, z):
+			self.z = z
+			y = np.where(z > 0, z, self.alpha * z)
+			return(y)
+		def backward(self, DL_DY):
+			DA_Dz = DL_DY.copy()
+			DA_Dz[self.z <= 0] *= self.alpha
+			return(DA_Dz)
+	class TanH():
+		''' The Hyperbolic Tangent (TanH) activation function '''
+		def forward(self, z):
+			self.y = np.tanh(z)
+			return(self.y)
+		def backward(self, DL_DY):
+			DT_Dz = (1 - self.y ** 2)
+			return(DL_DY * DT_Dz)
+	class Softmax():
+		''' The Softmax activation function '''
+		def forward(self, z):
+			exp_values = np.exp(z - np.max(z, axis=1, keepdims=True))
+			self.y = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+			return(self.y)
+		def backward(self, DL_DY):
+			DS_Dz = np.empty_like(DL_DY)
+			for i, (y, dy) in enumerate(zip(self.y, DL_DY)):
+				y = y.reshape(-1, 1)
+				jacobian_matrix = np.diagflat(y) - np.dot(y, y.T)
+				DS_Dz[i] = np.dot(jacobian_matrix, dy)
+			return(DS_Dz)
+	#---------- Accuracy Functions ---------- #
+	def Regression_Accuracy(self, y_true, y_pred):
+		''' Accuracy for regression models '''
+		accuracy_precision = np.std(y_true) / 250
+		predictions = y_pred
+		accuracy = np.mean(np.absolute(predictions-y_true) < accuracy_precision)
+		return(accuracy)
+	def Binary_Accuracy(self, y_true, y_pred):
+		''' Accuracy for binary classification models '''
+		predictions = (y_pred > 0.5) * 1
+		accuracy = np.mean(predictions == y_true)
+		return(accuracy)
+	def Categorical_Accuracy(self, y_true, y_pred):
+		''' Accuracy for categorical classification models '''
+		predictions = np.argmax(y_pred, axis=1)
+		accuracy = np.mean(predictions == y_true)
+		return(accuracy)
+	#---------- Loss Functions ---------- #
+	class BCE_Loss():
+		''' The Binary Cross-Entropy loss function '''
+		def forward(self, y_true, y_pred):
+			y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
+			loss = -(y_true*np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+			return(loss)
+		def backward(self, y_true,  y_pred):
+			y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
+			DL_DY = (y_pred - y_true) / (y_pred * (1 - y_pred)) / len(y_pred)
+			return(DL_DY)
+	class CCE_Loss():
+		''' The Categorical Cross-Entropy loss '''
+		def forward(self, y_true, y_pred):
+			y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
+			y_pred_vector = y_pred[range(len(y_pred)), y_true]
+			loss = -np.log(y_pred_vector)
+			return(loss)
+		def backward(self, y_true,  y_pred):
+			if len(y_true.shape) == 1:
+				y_true = np.eye(len(y_pred[0]))[y_true]
+			DL_DY = (-y_true / y_pred) / len(y_pred)
+			return(DL_DY)
+	class MSE_Loss():
+		''' The Mean Squared Error loss '''
+		def forward(self, y_true, y_pred):
+			loss = (y_true - y_pred)**2
+			return(loss)
+		def backward(self, y_true, y_pred):
+			DL_DY = (-2 * (y_true - y_pred) / len(y_pred[0])) / len(y_pred)
+			return(DL_DY)
+	class MAE_Loss():
+		''' The Mean Absolute Error loss '''
+		def forward(self, y_true, y_pred):
+			loss = np.abs(y_true - y_pred)
+			return(loss)
+		def backward(self, y_true, y_pred):
+			DL_DY = (np.sign(y_true - y_pred) / len(y_pred[0])) / len(y_pred)
+			return(DL_DY)
+	#---------- Optimisers ---------- #
+	def SGD(self, lr, layer):
+		''' The Stochastic Gradient Descent optimiser '''
+		layer.w -= lr * layer.dL_dw
+		layer.b -= lr * layer.dL_db
+
+
+
+
 	#---------- Layers ----------#
 	class Dense():
 		''' A dense layer '''
-		def __init__(self, inputs=1, outputs=1, alg='he uniform', sd=0.1, a=-0.5, b=0.5):
+		def __init__(self,
+					inputs=1, outputs=1,
+					alg='he uniform', sd=0.1, a=-0.5, b=0.5):
 			''' Initialise parameters '''
-			self.w, self.b, self.gamma, self.beta = self.Parameters(inputs, outputs, alg=alg, sd=sd, a=a, b=b)
+			self.w, self.b, self.gamma, self.beta = \
+			self.Parameters(inputs, outputs, alg=alg, sd=sd, a=a, b=b)
 			self.dL_dw, self.dL_db, self.dL_dx = None, None, None
-		def Parameters(self, inputs=1, outputs=1, alg='he uniform', sd=0.1, a=-0.5, b=0.5):
+		def Parameters(self,
+					inputs=1, outputs=1,
+					alg='he uniform', sd=0.1, a=-0.5, b=0.5):
 			''' Parameter Initialisation '''
 			if alg == 'zeros':
 				w = np.zeros((inputs, outputs))
@@ -108,16 +217,19 @@ class PyNN():
 			beta = np.zeros((1, outputs))
 			return(w, b, gamma, beta)
 		def forward(self, x):
-			''' The forward pass '''
 			self.x = x
 			z = np.dot(self.x, self.w) + self.b
 			return(z)
 		def backward(self, DA_Dz):
-			''' The backward pass '''
 			self.dL_dw = np.dot(self.x.T, DA_Dz)
 			self.dL_db = np.sum(DA_Dz, axis=0, keepdims=True)
 			self.dL_dx = np.dot(DA_Dz, self.w.T)
 			return(self.dL_dx)
+
+
+
+
+
 
 
 
@@ -130,6 +242,7 @@ class PyNN():
 			loss_fn = self.BCE_Loss()
 		for epoch in range(epochs):
 		# divide into Mini-Baches
+#		steps = X.shape[0] // batch_size
 		# steps in mini batches
 
 
@@ -159,6 +272,8 @@ class PyNN():
 
 
 
+#	''' Train on training set, then validate on valid, after training test of test set '''
+#	''' Add a training checkpoint '''
 
 
 
