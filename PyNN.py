de5df54b6ad88d40ba397e339f5b448ff874ef49
slurm <sari.sabban@gmail.com>
2025-03-25 16:39:58 +0300

    added regularisation but still needs fixing

--- PyNN.py
+++ PyNN.py
@@ -30,6 +30,7 @@ class PyNN():
 #		pickle.dump(self.w, f)
 #	def predict(self):
 #	''' Perform a prediction '''
+#	run training loop without backprop
 #	def GPU(self):
 #	''' Use GPU instead of CPU '''
 
@@ -187,7 +188,6 @@ class PyNN():
 		layer.b -= (lr / (np.sqrt(self.cache[layer]['b']) + e)) * layer.dL_db
 	def RMSprop(self, lr, decay, iters, beta, e, layer):
 		''' The RMSprop optimiser '''
-		# Initialize cache if not already
 		lr = lr * (1. / (1. + decay * iters))
 		self.cache = {}
 		if layer not in self.cache:
@@ -213,6 +213,82 @@ class PyNN():
 		b_c_c = layer.b_c / (1 - beta2 ** (iters + 1))
 		layer.w -= lr * w_m_c / (np.sqrt(w_c_c) + e)
 		layer.b -= lr * b_m_c / (np.sqrt(b_c_c) + e)
+
+
+
+
+
+
+	#---------- Regularisation ----------#
+	class Dropout():
+		''' The dropout regularisation layer '''
+		def __init__(self, p=0.25):
+			self.p = p
+		def forward(self, y, train=True):
+			if train:
+				self.mask = np.random.binomial(1, 1-self.p, y.shape)/(1-self.p)
+				y *= self.mask
+			return(y)
+		def backward(self, DA_Dz):
+			DA_Dz *= self.mask
+			return(DA_Dz)
+	class BatchNorm():
+		''' The Batch Normalisation regularisation layer '''
+		def __init__(self, g=1.0, b=0.0, e=1e-7):
+			self.w = g # replace g with w for optimiser
+			self.b = b
+			self.e = e
+		def forward(self, z):
+			self.z = z
+			self.mean = np.mean(z, axis=0, keepdims=True)
+			self.var = np.var(z, axis=0, keepdims=True)
+			self.z_norm = (self.z - self.mean) / np.sqrt(self.var + self.e)
+			z_new = self.w * self.z_norm + self.b
+			return(z_new)
+		def backward(self, DA_Dz):
+			m = self.z.shape[0]
+			self.dL_dw = np.sum(DA_Dz * self.z_norm, axis=0, keepdims=True) # dg
+			self.dL_db = np.sum(DA_Dz, axis=0, keepdims=True) # db
+			self.dB_dz = (self.w * (1./np.sqrt(self.var + self.e)) / m) * (m * DA_Dz - np.sum(DA_Dz, axis=0)
+			- (1./np.sqrt(self.var + self.e))**2 * (self.z - self.mean) * np.sum(DA_Dz*(self.z - self.mean), axis=0))
+			return(self.dB_dz)
+
+
+
+	class L1L2():
+		''' L1 + L2 regularisation '''
+		def forward(self, w, b, l1w, l1b, l2w, l2b):
+			self.w = w
+			self.b = b
+			self.L1w = l1w * np.sum(np.abs(w))
+			self.L1b = l1b * np.sum(np.abs(b))
+			self.L2w = l2w * np.sum(w**2)
+			self.L2b = l2b * np.sum(b**2)
+			L1L2 = self.L1w + self.L1b + self.L2w + self.L2b
+			return(L1L2)
+		def backward(self, dL_dw, dL_db):
+			dL1_dw = np.ones_like(w)
+			dL1_dw[w < 0] = -1
+			dL_dw += self.l1w * dL1_dw
+			dL1_db = np.ones_like(b)
+			dL1_db[b < 0] = -1
+			dL_db += self.l1b * dL1_db
+			dL_dw += 2 * self.l2w * w
+			dL_db += 2 * self.l2b * b
+			return(dL_dw, dL_db)
+
+
+
+
+
+
+
+
+
+
+
+
+
 	#---------- Layers ----------#
 	class Dense():
 		''' A dense layer '''
@@ -263,6 +339,26 @@ class PyNN():
 			self.dL_db = np.sum(DA_Dz, axis=0, keepdims=True)
 			self.dL_dx = np.dot(DA_Dz, self.w.T)
 			return(self.dL_dx)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	#---------- Training ---------- #
 	def train(self,
 			X_train=None, Y_train=None,
@@ -314,7 +410,8 @@ class PyNN():
 				# Forward propagation
 				output = X_train_batch
 				y_true = Y_train_batch
-				for layer in self.layers: output = layer.forward(output)
+				for layer in self.layers:
+					output = layer.forward(output)
 				y_pred = output
 				cost_train = self.cost(loss_fn.forward(y_true, y_pred))
 				# Accuracy calculation
@@ -326,7 +423,7 @@ class PyNN():
 					grad = self.layers[i].backward(grad)
 				# Gradient descent
 				for layer in self.layers:
-					if isinstance(layer, self.Dense):
+					if isinstance(layer, (self.Dense, self.BatchNorm)):
 						if optimiser.lower() == 'sgd':
 							self.SGD(lr, decay, epoch, layer)
 						elif optimiser.lower() == 'adagrad':
@@ -454,6 +551,7 @@ X_valid, X_tests, Y_valid, Y_tests = sklearn.model_selection.train_test_split(X_
 
 model = PyNN()
 model.add(model.Dense(2, 64))
+model.add(model.BatchNorm())
 model.add(model.ReLU())
 model.add(model.Dense(64, 1))
 model.add(model.Sigmoid())
@@ -461,8 +559,8 @@ model.add(model.Sigmoid())
 model.train(
 		X_train, 
 		Y_train, 
-		X_valid, 
-		Y_valid, 
+#		X_valid, 
+#		Y_valid, 
 #		X_tests, 
 #		Y_tests, 
 #		batch_size=16, 
