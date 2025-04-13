c615ff4e6f84ad4985006b3d85cc26c42b85147f
slurm <sari.sabban@gmail.com>
2025-04-13 10:01:48 +0300

    working on the backward pass

--- pynn/PyNN.py
+++ pynn/PyNN.py
@@ -594,7 +594,7 @@ class PyNN():
 
 
 
-	class Pool():
+	class Pool(): ##### add GPU
 		''' An n dimensional pooling layer '''
 		def __init__(self, window=(2, 2), stride=(2, 2), alg='max'):
 			if isinstance(window, int): self.window = (window,)
@@ -603,6 +603,7 @@ class PyNN():
 			else: self.stride = stride
 			self.alg = alg.lower()
 		def forward(self, x):
+			self.x = x
 			d, w, s = x.shape, self.window, self.stride
 			stds = x.strides
 			output_shape = tuple((d[i]-w[i]) // s[i] + 1 for i in range(len(d)))
@@ -615,21 +616,56 @@ class PyNN():
 			if   self.alg == 'max': output = np.max(self.windows,  axis=axis)
 			elif self.alg == 'avg': output = np.mean(self.windows, axis=axis)
 			return(output)
-#		def backward(self, dz):
+		def backward(self, dz):
+
+
+
+
+
+
+
+
+			dx = np.zeros_like(self.x)
+			if self.alg == 'max':
+				idx = np.argmax(self.windows.reshape(dz.shape + (-1,)), axis=-1)
+				coords = np.unravel_index(idx, self.window)
+				meshgrid = np.meshgrid(*[np.arange(s) for s in dz.shape], indexing='ij')
+				indices = [meshgrid[i] * self.stride[i] + coords[i] for i in range(len(self.stride))]
+				np.add.at(dx, tuple(indices), dz)
+			elif self.alg == 'avg':
+				meshgrid = np.meshgrid(*[np.arange(s) for s in dz.shape], indexing='ij')
+				indices = [meshgrid[i] * self.stride[i] + np.arange(self.window[i])[:, np.newaxis] for i in range(len(self.stride))]
+				np.add.at(dx, tuple(indices), dz[..., np.newaxis] / np.prod(self.window))
+			return(dx)
 
 
 
 
 model = PyNN()
-
+#dz = np.array([1, 2, 3, 4])
 #X = np.array([1, 3, 2, 5, 7, 6, 1, 4]) # [3, 5, 7, 4]
-#model.add(model.Pool(2, 2, 'max'))
+#model.add(model.Pool(2, 2, 'max'))     # [0 1 0 2 3 0 0 4]
 
+#dz = np.array([[1, 2], [3, 4]])
 #X = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
 #model.add(model.Pool((2, 2), (2, 2), 'max'))
+'''
+[[ 6  8]
+ [14 16]]
 
-X = np.array([[[ 1,  2,  3,  4], [ 5,  6,  7,  8], [ 9, 10, 11, 12], [13, 14, 15, 16]],[[17, 18, 19, 20], [21, 22, 23, 24], [25, 26, 27, 28], [29, 30, 31, 32]],[[33, 34, 35, 36], [37, 38, 39, 40], [41, 42, 43, 44], [45, 46, 47, 48]],[[49, 50, 51, 52], [53, 54, 55, 56], [57, 58, 59, 60], [61, 62, 63, 64]]])
-model.add(model.Pool((2, 2, 2), (2, 2, 2), 'max'))
+[[0 0 0 0]
+ [0 1 0 2]
+ [0 0 0 0]
+ [0 3 0 4]]
+'''
+
+#dz = np.array([])
+#X = np.array([[[ 1,  2,  3,  4], [ 5,  6,  7,  8], [ 9, 10, 11, 12], [13, 14, 15, 16]],[[17, 18, 19, 20], [21, 22, 23, 24], [25, 26, 27, 28], [29, 30, 31, 32]],[[33, 34, 35, 36], [37, 38, 39, 40], [41, 42, 43, 44], [45, 46, 47, 48]],[[49, 50, 51, 52], [53, 54, 55, 56], [57, 58, 59, 60], [61, 62, 63, 64]]])
+#model.add(model.Pool((2, 2, 2), (2, 2, 2), 'max'))
+'''
+'''
 
 y = model.layers[0].forward(X)
 print(y)
+dx = model.layers[0].backward(dz)
+print(dx)
